85910cc8ba7efbee737de0b24d16e8333c2905d2
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-07-05 10:38:21 -0400

    ADD modules from hilt

---
+++ app/src/main/java/weatherapp/ca/di/LocationModule.kt
@@ -0,0 +1,21 @@
+package weatherapp.ca.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import weatherapp.ca.data.location.DefaultLocationTracker
+import weatherapp.ca.domain.location.LocationTracker
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(
+        defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/weatherapp/ca/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package weatherapp.ca.di
+
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import weatherapp.ca.data.repository.WeatherRepositoryImpl
+import weatherapp.ca.domain.repository.WeatherRepository
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
